690c7559807d25de4b0ceefd01753ce669cd9f1e
Pramendra10000 <pramendra10000.com>
2025-09-21 01:08:37 +0530

    Added fixex error

--- src/Components/FlotingDiv/FlotingDiv.jsx
+++ src/Components/FlotingDiv/FlotingDiv.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import "./FlotingDiv.css";
 
-<<<<<<< HEAD
 const FlotingDiv = ({ image, txt1, txt2 }) => {
-=======
-import { themeContext } from "../../Context.js";
-import { useContext } from "react";
-
-const FlotingDiv = ({image ,txt1 ,txt2}) => {
->>>>>>> cfad1b054ff002f6bfe99a87dd127c63afd7e673
   return (
     <div>
-      <div className="floatingcontent" >
+      <div className="floatingcontent">
         <img src={image} alt="" />
         <span>
           {txt1}
