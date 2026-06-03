8724f35acff58c91bce9b2567d6270c3bd151fa9
Pramendra10000 <pramendra10000@gmail.com>
2026-06-03 13:47:03 +0530

    testing .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "./Context";
 import App from "./App";
 import JavaCoding from "./Components/StudyTime/javacoding";
